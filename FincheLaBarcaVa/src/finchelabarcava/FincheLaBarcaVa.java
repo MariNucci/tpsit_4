e48badb4bd55309132275ec2a44aed0a35d837e5
studente <studente@Mat-27>
2017-04-20 10:11:33 +0200

    FincheLaBarcaVa

---
+++ FincheLaBarcaVa/src/finchelabarcava/FincheLaBarcaVa.java
@@ -0,0 +1,10 @@
+package finchelabarcava;
+
+public class FincheLaBarcaVa {
+
+    public static void main(String[] args) {
+        Finestra f=new Finestra();
+        f.setVisible(true);
+    }
+    
+}
